4597e03b2ea7a9fd6c147a0c404824baba4d5924
Matt Cibelli <mattcibelli@UXA-MacBook-Pro.local>
2023-05-04 19:57:46 -0700

    change to app.py db.create_all

--- app.py
+++ app.py
@@ -92,8 +92,9 @@ def create_app(db_url=None):
             401,
         )
 
-    #with app.app_context():
-       # db.create_all()
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
